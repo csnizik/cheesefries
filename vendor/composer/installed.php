25a46bfa01601041aa6e0757e5d2349910d7ae34
csnizik <csnizik@tulane.edu>
2025-02-11 11:03:35 -0600

    themey things

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -172,6 +172,15 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'drupal/admin_toolbar' => array(
+            'pretty_version' => '3.5.1',
+            'version' => '3.5.1.0',
+            'reference' => '3.5.1',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../web/modules/contrib/admin_toolbar',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'drupal/classy' => array(
             'pretty_version' => '1.0.2',
             'version' => '1.0.2.0',
